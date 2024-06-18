793db69ef8e24a47133c01bc49b1494b140f9e76
Devon Colmer <935806+d3vco@users.noreply.github.com>
2024-06-18 12:55:18 -0400

    feat: add v5 ui

--- app/dnp3_svc.py
+++ app/dnp3_svc.py
@@ -1,8 +1,39 @@
 import logging
 
+from aiohttp import web
+from aiohttp_jinja2 import template
+
 
 class Dnp3Service:
-    def __init__(self, services):
+    def __init__(self, services, name, description):
+        self.name = name
+        self.description = description
         self.services = services
-        self.file_svc = services.get('file_svc')
-        self.log = logging.getLogger('dnp3_svc')
+
+        self.data_svc = services.get('data_svc')
+        self.log = logging.getLogger("dnp3_svc") 
+
+    @template('dnp3.html')
+    async def splash(self, request):
+        data = await self._get_plugin_data()
+        return data 
+    
+    async def plugin_data(self, request):
+        data = await self._get_plugin_data()
+        return web.json_response(data)
+
+    async def _get_plugin_data(self):
+        # Collapse abilities by their ability IDs - this fixes issue of duplicates as locate gives executor variants
+        abilities = {
+            a.ability_id: {
+                "name"       : a.name,
+                "tactic"     : a.tactic,
+                "technique_id": a.technique_id,
+                "technique_name": a.technique_name,
+                "description": a.description.replace('\n', '<br>')  # nicer display
+            }
+            for a in await self.data_svc.locate('abilities')
+            if await a.which_plugin() == 'dnp3'
+        }
+        abilities = list(abilities.values())
+        return dict(name=self.name, description=self.description, abilities=abilities)
--- hook.py
+++ hook.py
@@ -1,6 +1,5 @@
 from app.utility.base_world import BaseWorld
-from plugins.dnp3.app.dnp3_gui import Dnp3GUI
-from plugins.dnp3.app.dnp3_api import Dnp3API
+from plugins.dnp3.app.dnp3_svc import Dnp3Service
 
 name = 'DNP3'
 description = 'The DNP3 plugin for Caldera provides adversary emulation abilities specific to the DNP3 control systems protocol.'
@@ -9,12 +8,7 @@ access = BaseWorld.Access.RED
 
 
 async def enable(services):
+    dnp3_svc = Dnp3Service(services, name, description)
     app = services.get('app_svc').application
-    dnp3_gui = Dnp3GUI(services, name=name, description=description)
-    app.router.add_static('/dnp3', 'plugins/dnp3/static/', append_version=True)
-    app.router.add_route('GET', '/plugin/dnp3/gui', dnp3_gui.splash)
-
-    dnp3_api = Dnp3API(services)
-    # Add API routes here
-    app.router.add_route('POST', '/plugin/dnp3/mirror', dnp3_api.mirror)
-
+    app.router.add_route('GET', '/plugin/dnp3/gui', dnp3_svc.splash)
+    app.router.add_route('GET', '/plugin/dnp3/data', dnp3_svc.plugin_data)
